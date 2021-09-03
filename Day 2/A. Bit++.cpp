33552b7b925cfd2a47c54a164620b89c73c6a6f4
Kanto Bhattacharjee <kanto.uv@outlook.com>
2021-09-04 02:39:57 +0600

    update

---
+++ Day 2/A. Bit++.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int x=0,n;
+    string s;
+    cin>>n;
+
+    for(int i=0; i<n; i++){
+        cin>>s;
+        if(s[1]=='+'){
+            x++;
+        }
+        else
+            x--;
+    }
+    cout<<x;
+
+}
