7ee041e1db72449d313a8cc82686d47e91e1f430
Kanto Bhattacharjee <kanto.uv@outlook.com>
2021-09-07 10:30:39 +0600

    update

---
+++ Day 5/A. Anton and Danik.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n,a=0,d=0;
+    string s;
+    cin>>n;
+    cin>>s;
+    for(int i=0; i<n; i++){
+        if(s[i]=='A'){
+            a++;
+        }
+        else if(s[i]=='D'){
+            d++;
+        }
+    }
+    if(a>d){
+        cout<<"Anton";
+    }
+    else if(d>a){
+        cout<<"Danik";
+    }
+    else
+        cout<<"Friendship";
+
+}
